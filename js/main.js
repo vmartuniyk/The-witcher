041534c8b794d0a6f359a56cdb409962151b953d
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-02-06 21:12:52 +0100

    Adaptive for mobile and mobile menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
  var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
-   slidesPerView: 2,
+   slidesPerView: 1,
    loop: true,
       navigation: {
         nextEl: '.arrow',
       },
+       breakpoints: {
+    
+        640: {
+          slidesPerView: 2
+        }
+      }
   })
+ 
+ var menuButton = document.querySelector('.menu-button');
+ var menu = document.querySelector('.header');
+    menuButton.addEventListener('click',function(){
+        menuButton.classList.toggle('menu-button-active');
+        menu.classList.toggle('header-active');
+    
+});
